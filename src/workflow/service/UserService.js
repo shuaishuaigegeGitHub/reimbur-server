d3705bd6812642ab36e1dd4cd94846db477780b9
ljw <lianjianwei@fenglinghudong.com>
2021-02-25 21:09:03 +0800

    :fire: 移除无用代码

--- src/workflow/service/UserService.js
+++
@@ -1,9 +0,0 @@
-export default class UserService {
-    /**
-     * 查询任务执行者
-     * @param {object} workflowInstance 流程实例
-     * @param {TaskModel} nodeModel 任务节点模型
-     * @param {object} param 流程实例参数
-     */
-    async queryTaskPerformer(workflowInstance, nodeModel, param) {}
-}
